5e02b4dc9b384b1d92fae028996dd7a9b77c8ca6
OmarAyman33 <omaraymanhameds@gmail.com>
2025-04-13 23:08:04 +0200

    added abstract user class.

---
+++ src/User.java
@@ -0,0 +1,52 @@
+enum Gender {
+    MALE, FEMALE, OTHER
+}
+
+public abstract class User {
+    protected String username;
+    protected String password;
+    protected String dateOfBirth;
+    protected Gender gender;
+
+    // constructors
+    public User(String username, String password, String dateOfBirth, Gender gender) {
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+
+    public User(){}
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    // Setters
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // centralized log in function as all loginable functions inherit from this class
+    public boolean login(String username, String password){
+        if (username == null || password == null) return false; // checking for nullptr exception
+
+        if(this.username.equals(username))
+        {
+            if(this.password.equals(password.strip()))
+                return true;
+        }
+        return false;
+    }
+
+    public void displayDashboard(){}
+}
