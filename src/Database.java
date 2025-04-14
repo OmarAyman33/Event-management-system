16ed4c5fc10d1c78bbc4c01182ab476615c71970
OmarAyman33 <omaraymanhameds@gmail.com>
2025-04-14 23:01:22 +0200

    added database and LoginAuth.

---
+++ src/Database.java
@@ -0,0 +1,7 @@
+import java.util.List;
+public class Database {
+    // dummy data
+    public static List<User>  users;
+    public static List<Event> events;
+    public static List<Room> rooms;
+}
